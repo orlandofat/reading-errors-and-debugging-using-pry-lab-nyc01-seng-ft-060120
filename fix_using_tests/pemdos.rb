3c7569fe2da6d2a90a4f902bf4c6d9bea8e58a53
Orlando Challenger <orlandofat@gmail.com>
2020-05-13 16:37:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
+  if string[0] = "s"
     #binding.pry
-     (10 * "s".to_i).to_s +""+ string
+     (10 * "s".to_i).to_s + string
   else
     string
   end
